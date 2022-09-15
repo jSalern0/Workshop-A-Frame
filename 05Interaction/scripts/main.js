f4bd08a0fc123608c8104e52195bba9d6a77f7e3
jSalern0 <salerno.jullya@hotmail.com>
2022-09-15 16:40:36 +0200

    trying onmouseleave

--- 05Interaction/scripts/main.js
+++ 05Interaction/scripts/main.js
@@ -17,7 +17,6 @@ setInterval(spin, 16);
  	console.log('mouse enter');
  });
 
-
- myOtherBox.addEventListener('mouseleave', function(){
- rotationSpeed = 0.001;
- onmouseleave = (function) => { });
+myOtherBox.onmouseleave = function(){
+	rotationSpeed = 0.001;
+}
